281f56442ff672fa6a97cce89f414129b645cbd5
Oscar <oorellana30.95@gmail.com>
2022-10-19 19:13:06 -0500

    Add Hello CDK Lambda

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,11 +1,8 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    # We are importing the aws_lambda module as _lambda because lambda is a built-in identifier in Python.
+    aws_lambda as _lambda
 )
 
 
@@ -13,14 +10,14 @@ class CdkWorkshopStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
+        my_lambda = _lambda.Function(
+            self,
+            'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            # The handler code is loaded from the lambda directory which we created earlier.
+            # Path is relative to where you execute cdk from, which is the project’s root directory
+            code=_lambda.Code.from_asset('lambda'),
+            # The name of the handler function is hello.handler (“hello” is the name of the file and “handler”
+            # is the function name)
+            handler='hello.handler'
         )
-
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
